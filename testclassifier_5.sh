ae7d66b802c2f047701969f69e816d0a3285924e
calipos <calipos@126.com>
2016-10-11 20:16:45 +0800

    Create testclassifier_5.sh

---
+++ testclassifier_5.sh
@@ -0,0 +1,6 @@
+ /nn/caffe-master/build/examples/cpp_classification/classification.bin \
+  /imageDB/prototxt/lenet.prototxt \
+  /imageDB/caffe_result/lenet_iter_5000.caffemodel \
+  /imageDB/mean.binaryproto \
+  /imageDB/flags.txt \
+  /imageDB/test_jpg.jpg
