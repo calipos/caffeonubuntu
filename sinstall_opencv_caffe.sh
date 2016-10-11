ebba8dc495286fba9448584d90a7d516ba44ca91
calipos <calipos@126.com>
2016-10-11 13:17:11 +0800

    Create sinstall_opencv_caffe.sh

---
+++ sinstall_opencv_caffe.sh
@@ -0,0 +1,20 @@
+set -e
+cd /nn/opencv/
+tar -xvf opencv-2.4.13.zip
+
+cd opencv-2.4.13/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make -j 4
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+echo "OpenCV" $version "ready to be used"
+
+
+cd /nn/caffeonubuntu/
+cp Makefile.config /nn/caffe-master/
+make all -j4
+make test
+make runtest
