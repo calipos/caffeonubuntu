c3cb2099826e75a30b3fddddca5d28b5f42a19b9
calipos <calipos@126.com>
2016-10-11 13:01:40 +0800

    Update sinstall.sh

--- sinstall.sh
+++ sinstall.sh
@@ -1,3 +1,4 @@
+set -e
 cp /etc/apt/sources.list /etc/apt/sources.list.old
 Codename=$(lsb_release -a | grep Codename | awk '{print $2}')
 echo "\
@@ -13,12 +14,10 @@ deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-security main multiverse res
 deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-updates main multiverse restricted universe ">/etc/apt/sources.list
 apt-get update
 apt-get upgrade
-apt-get install -y build-essential cmake  pkg-config
 apt-get install libprotobuf
 apt-get install libprotobuf-dev
 apt-get install libleveldb-dev
 apt-get install libsnappy-dev
-apt-get install libopencv-dev
 apt-get install libboost-all-dev
 apt-get install libhdf5-serial-dev
 apt-get install libgflags-dev
@@ -29,7 +28,6 @@ apt-get install python2.7
 apt-get install python2.7-dev
 apt-get install python-pip
 apt-get install -y libatlas-base-dev
-apt-get install  -y python-numpy
 apt-get install  -y python-scipy
 apt-get install  -y python-matplotlib
 apt-get install  -y python-sklearn python-skimage
@@ -42,10 +40,15 @@ apt-get install  -y ipython
 apt-get install  -y python-yaml
 apt-get install  -y protobuf-c-compiler 
 apt-get install  -y protobuf-compile
+sh ./opencv_dependencies.sh
 mkdir nn
 cd nn/
 mkdir opencv
 cd opencv/
 apt-get install openssh-server
-reboot
-#next copy the caffe file and the opencv2.4.13 to the dir:/nn and /nn/opencv/
+
+echo "--- Plz test WinSCP , local ip is "
+ifconfig |grep "inet addr"
+echo "---if winscp cannot connect, try reboot"
+echo "---else copy the caffe file and the opencv2.4.13 to the dir:/nn and /nn/opencv/ "
+
