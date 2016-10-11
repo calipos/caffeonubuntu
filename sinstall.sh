613efa7b875f6fde198b9da67d19659762b99f63
calipos <calipos@126.com>
2016-10-11 15:12:25 +0800

    Update sinstall.sh

--- sinstall.sh
+++ sinstall.sh
@@ -15,31 +15,54 @@ deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-updates main multiverse rest
 apt-get update
 apt-get upgrade
 # apt-get install libprotobuf
+echo "------ install libprotobuf ------"
 apt-get install -y  libprotobuf-dev
+echo "------ install libleveldb ------"
 apt-get install -y  libleveldb-dev
+echo "------ install libsnappy ------"
 apt-get install -y  libsnappy-dev
+echo "------ install libboost ------"
 apt-get install -y  libboost-all-dev
+echo "------ install libhdf5 ------"
 apt-get install  -y libhdf5-serial-dev
+echo "------ install libgflags ------"
 apt-get install -y  libgflags-dev
+echo "------ install libgoogle ------"
 apt-get install -y  libgoogle-glog-dev
+echo "------ install liblmdb ------"
 apt-get install -y  liblmdb-dev
+echo "------ install protobuf ------"
 apt-get install -y  protobuf-compiler
+echo "------ install python2 ------"
 apt-get install -y  python2.7 
+echo "------ install python2 ------"
 apt-get install -y  python2.7-dev
+echo "------ install pip ------"
 apt-get install -y  python-pip
+echo "------ install libatlas ------"
 apt-get install -y libatlas-base-dev
+echo "------ install scipy ------"
 apt-get install  -y python-scipy
+echo "------ install matplotlib ------"
 apt-get install  -y python-matplotlib
+echo "------ install sklearn ------"
 apt-get install  -y python-sklearn python-skimage
+echo "------ install python-h5py ------"
 apt-get install  -y python-h5py
+echo "------ install python-protobuf ------"
 apt-get install  -y python-protobuf
+echo "------ install python-leveldb ------"
 apt-get install  -y python-leveldb python-networkx python-nose python-pandas 
+echo "------ install gflags ------"
 apt-get install  -y python-gflags
+echo "------ install cython ------"
 apt-get install  -y cython
+echo "------ install ipython ------"
 apt-get install  -y ipython
+echo "------ install python-yaml ------"
 apt-get install  -y python-yaml
+echo "------ install protobuf-c-compiler ------"
 apt-get install  -y protobuf-c-compiler 
-apt-get install  -y protobuf-compiler
 sh ./opencv_dependencies.sh
 cd /
 mkdir nn
