ed33b3f0d9fa0ed86e666aa1ca9e78d326b0d14f
calipos <calipos@126.com>
2018-01-28 20:36:42 +0800

    Update sinstall_opencv_caffe_2.sh

--- sinstall_opencv_caffe_2.sh
+++ sinstall_opencv_caffe_2.sh
@@ -5,7 +5,7 @@ tar -xvf opencv-2.4.13.zip
 cd opencv-2.4.13/
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D BUILD_opencv_gpu=OFF ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D BUILD_opencv_gpu=OFF -D BUILD_opencv_flann=OFF -D BUILD_opencv_objdetect=OFF -D BUILD_opencv_ml=OFF  -D BUILD_opencv_photo=OFF -D BUILD_opencv_video=OFF -D BUILD_opencv_superres=OFF   ..
 make -j 4
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
